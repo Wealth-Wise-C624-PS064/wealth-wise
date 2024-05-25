834fb1134ae9e497cb657a012465d9e447d44ea2
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-25 17:46:47 +0700

    feat: changing responsiveness of header calculator page

--- src/pages/calculator/calculator-page.jsx
+++ src/pages/calculator/calculator-page.jsx
@@ -5,15 +5,19 @@ function CalculatorPage() {
   return (
     <BaseLayout>
       <section className="bg-primary-blue ">
-        <div className="flex flex-col items-center justify-center h-full max-w-6xl px-4 py-12 mx-auto md:gap-20 md:flex-row min-h-96">
+        <div className="flex flex-col items-center justify-center h-full max-w-6xl px-4 md:px-16 py-12 mx-auto md:gap-20 md:flex-row min-h-96">
           <div className="space-y-4">
-            <h2 className="text-3xl font-bold leading-snug text-white md:text-5xl">
+            <h2 className="text-5xl font-bold leading-snug text-white xl:text-6xl">
               Kalkulator
             </h2>
             <div className="space-y-4">
-              <p className="text-2xl text-white">Untuk Hitung Investasi</p>
-              <p className="text-2xl text-white">Untuk Hitung Dana Darurat</p>
-              <p className="text-2xl text-white pb-6">
+              <p className="text-4xl font-semibold text-white">
+                Untuk Hitung Investasi
+              </p>
+              <p className="text-4xl font-semibold text-white">
+                Untuk Hitung Dana Darurat
+              </p>
+              <p className="text-4xl font-semibold text-white pb-6">
                 Untuk Hitung Dana Pensiun
               </p>
             </div>
