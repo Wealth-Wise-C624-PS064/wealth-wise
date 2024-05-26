b032225d5154ee66a82ed7397ee752d6defdac41
vitohartanto <72955373+vitohartanto@users.noreply.github.com>
2024-05-26 19:48:40 +0700

    feat: adding logics to emergencyFund and pensionFund (#12)
    * feat: adding calculator routing
    
    * fix: missing / on path
    
    * feat: adding calculator page
    
    * feat: changing header of calculator page
    
    * feat: changing responsiveness of header calculator page
    
    * feat: creating UI for the calculator's option link
    
    * feat: slicing UI Investment Page
    
    * feat: giving responsiveness to investment page
    
    * feat: creating part of emergency-fund-page
    
    * feat: finishing emergency fund page
    
    * feat: additional changes in emergency fund page
    
    * feat: creating pension fund page
    
    * fixing UI for pension fund page
    
    * feat: create logics for investment page
    
    * giving logics to emergencyFundPage and pensionFundPage

--- src/pages/calculator/pension-fund-page.jsx
+++ src/pages/calculator/pension-fund-page.jsx
@@ -10,6 +10,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useState } from "react";
+import useInput from "@/hooks/useInput";
+import { toRupiah } from "@/lib/toRupiah";
 
 const descFormulaHandler = (event) => {
   event.preventDefault();
@@ -25,7 +28,7 @@ const descFormulaHandler = (event) => {
       Pertama, ketahui M sebagai Future Value dari Pengeluaran Bulanan
       </b>
       <h2>
-      M = P * (1 + i)<sup>t</sup>
+      M = MEL * (1 + i)<sup>t</sup>
       </h2>
       
       <br />
@@ -54,23 +57,62 @@ const descFormulaHandler = (event) => {
 };
 
 function PensionFundPage() {
+  const [monthlyExpensesLater, onChangeMonthlyExpensesLaterHandler] =
+    useInput("");
+  const [yearsLater, onChangeYearsLaterHandler] = useInput("");
+  const [inflation, onChangeInflationHandler] = useInput("");
+  const [annualReturn, onChangeAnnualReturnHandler] = useInput("");
+  const [pensionFund, setPensionFund] = useState(null);
+
+  const calculatePensionFund = (event) => {
+    event.preventDefault();
+
+    // removing the comma from the numbers and giving declarations
+    const MEL_value = parseFloat(monthlyExpensesLater.replace(/[,.]/g, ""));
+    const t_value = parseFloat(yearsLater);
+    const i_value = parseFloat(inflation) / 100;
+    const r_value = parseFloat(annualReturn) / 100;
+
+    if (
+      isNaN(MEL_value) ||
+      isNaN(t_value) ||
+      isNaN(i_value) ||
+      isNaN(r_value)
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Silakan masukkan semua nilai dengan benar!",
+      });
+      return;
+    }
+
+    const M_value = MEL_value * Math.pow(1 + i_value, t_value);
+    const Y_value = M_value * 12;
+    const pensionFundAmount = (100 / (r_value * 100 - i_value * 100)) * Y_value;
+
+    setPensionFund(pensionFundAmount.toFixed(2));
+  };
+
   return (
     <div className="sm:border-2 p-4 sm:p-8 lg:p-16 rounded-2xl mb-8">
       <h1 className="text-2xl font-bold mb-8">
         Rencanakan<span className="text-primary-blue"> Dana Pensiun</span> untuk
         Masa Tua Anda
       </h1>
-      <form>
+      <form onSubmit={calculatePensionFund}>
         <div className="flex flex-col mb-8">
           <label htmlFor="" className="font-bold text-xl mb-3">
             Rencana pengeluaran bulanan saat Anda pensiun nanti?{" "}
-            <span className="text-primary-blue">(P)</span>
+            <span className="text-primary-blue">(MEL)</span>
           </label>
           <div className="flex items-center">
             <p className="font-bold text-xl mr-4">Rp</p>
             <input
               type="text"
               placeholder="Contoh: 5.000.000"
+              value={monthlyExpensesLater}
+              onChange={onChangeMonthlyExpensesLaterHandler}
               className="w-3/5 px-4 py-2 border-primary-blue border-[3px] rounded-2xl"
             />
           </div>
@@ -85,6 +127,8 @@ function PensionFundPage() {
             <input
               type="text"
               placeholder="2"
+              value={yearsLater}
+              onChange={onChangeYearsLaterHandler}
               className="text-center w-1/5 px-4 py-2 border-primary-blue border-[3px] rounded-2xl mr-4"
             />
             <p className="font-bold text-xl ">tahun</p>
@@ -100,6 +144,8 @@ function PensionFundPage() {
             <input
               type="text"
               placeholder="5"
+              value={inflation}
+              onChange={onChangeInflationHandler}
               className="text-center w-1/5 px-4 py-2 border-primary-blue border-[3px] rounded-2xl mr-4"
             />
             <p className="font-bold text-xl ">%/ tahun</p>
@@ -115,6 +161,8 @@ function PensionFundPage() {
             <input
               type="text"
               placeholder="5"
+              value={annualReturn}
+              onChange={onChangeAnnualReturnHandler}
               className="text-center w-1/5 px-4 py-2 border-primary-blue border-[3px] rounded-2xl mr-4"
             />
             <p className="font-bold text-xl ">%/ tahun</p>
@@ -133,10 +181,13 @@ function PensionFundPage() {
         </div>
       </form>
 
-      <h1 className="font-bold text-2xl mb-12">
-        Berdasarkan 4% rule, Anda harus memiliki setidaknya Rp 13.147.001.734
-        sebagai Dana Pensiun
-      </h1>
+      {pensionFund && (
+        <h1 className="font-bold text-2xl mb-12">
+          Berdasarkan 4% rule, Anda harus memiliki setidaknya
+          {toRupiah(pensionFund)}
+          sebagai Dana Pensiun
+        </h1>
+      )}
 
       <button className="bg-primary-blue px-8 py-2 text-white rounded-2xl text-lg font-semibold mr-4 mb-8">
         Simpan ke Tabel
