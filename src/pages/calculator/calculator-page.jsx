ebf77ab6f19ce325cead87a4a3fed1ee9aa0f95d
vitohartanto <72955373+vitohartanto@users.noreply.github.com>
2024-05-26 13:29:25 +0700

    feat: adding investment page and emergency fund page (#9)
    * feat: adding calculator routing
    
    * fix: missing / on path
    
    * feat: adding calculator page
    
    * feat: changing header of calculator page
    
    * feat: changing responsiveness of header calculator page
    
    * feat: creating UI for the calculator's option link
    
    * feat: slicing UI Investment Page
    
    * feat: giving responsiveness to investment page
    
    * feat: creating part of emergency-fund-page
    
    * feat: finishing emergency fund page

--- src/pages/calculator/calculator-page.jsx
+++ src/pages/calculator/calculator-page.jsx
@@ -1,11 +1,16 @@
 import BaseLayout from "@/layouts/base-layout";
 import { Link, Outlet } from "react-router-dom";
+import {
+  FaMoneyBillTrendUp,
+  FaPersonCane,
+  FaTriangleExclamation,
+} from "react-icons/fa6";
 
 function CalculatorPage() {
   return (
-   <BaseLayout>
+ <BaseLayout>
       <section className="bg-primary-blue ">
-        <div className="flex flex-col items-center justify-center h-full max-w-6xl px-4 md:px-16 py-12 mx-auto md:gap-20 md:flex-row min-h-96">
+        <div className="mx-auto flex flex-col items-center justify-center h-full max-w-6xl px-4 md:px-16 py-12 md:gap-20 md:flex-row min-h-96">
           <div className="space-y-4">
             <h2 className="text-5xl font-bold leading-snug text-white xl:text-6xl">
               Kalkulator
@@ -32,12 +37,35 @@ function CalculatorPage() {
           </div>
         </div>
       </section>
-      <section className="max-w-6xl mx-auto px-4">
-        <Link to="/kalkulator/investasi">Kalkulator Investasi</Link>
-        <Link to="/kalkulator/dana-pensiun">Dana Pensiun</Link>
-        <Link to="/kalkulator/dana-darurat">Dana Darurat</Link>
+      <section className="max-w-6xl mx-auto px-3  flex pt-8">
+        <div className="flex space-x-2 sm:space-x-6 lg:space-x-12 mb-8 mx-auto">
+          <Link to="/kalkulator/investasi">
+            <div className="border-2 border-black  rounded-2xl w-28 h-28 md:w-[136px] md:h-[136px] lg:w-40 lg:h-40 flex flex-col items-center justify-center px-1 md:px-4">
+              <FaMoneyBillTrendUp className="text-4xl md:text-5xl lg:text-6xl pb-1" />
+              <p className="text-base md:text-lg lg:text-xl font-bold text-center">
+                Investasi
+              </p>
+            </div>
+          </Link>
+          <Link to="/kalkulator/dana-darurat">
+            <div className="border-2 border-black  rounded-2xl w-28 h-28 md:w-[136px] md:h-[136px] lg:w-40 lg:h-40 flex flex-col items-center justify-center px-1 md:px-4">
+              <FaTriangleExclamation className="text-4xl md:text-5xl lg:text-6xl pb-1" />
+              <p className="text-base md:text-lg lg:text-xl font-bold text-center">
+                Dana Darurat
+              </p>
+            </div>
+          </Link>
+          <Link to="/kalkulator/dana-pensiun">
+            <div className="border-2 border-black  rounded-2xl w-28 h-28 md:w-[136px] md:h-[136px] lg:w-40 lg:h-40 flex flex-col items-center justify-center px-1 md:px-4">
+              <FaPersonCane className="text-4xl md:text-5xl lg:text-6xl pb-1" />
+              <p className="text-base md:text-lg lg:text-xl font-bold text-center">
+                Dana Pensiun
+              </p>
+            </div>
+          </Link>
+        </div>
       </section>
-      <section className="max-w-6xl mx-auto px-4">
+      <section className="max-w-6xl mx-auto px-4 sm:px-8 md:px-24">
         <Outlet />
       </section>
     </BaseLayout>
--- src/pages/calculator/emergency-fund-page.jsx
+++ src/pages/calculator/emergency-fund-page.jsx
@@ -1,5 +1,153 @@
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const descFormulaHandler = (event) => {
+  event.preventDefault();
+  Swal.fire({
+    color: "#000",
+    background: "rgba(255,255,255, 0.95)",
+    backdrop: `rgba(42,157,242,0.4)`,
+    confirmButtonColor: "#16db3d",
+    title: `<strong>Formula Hitung <span style="color:#2A9DF2;">Dana Darurat</span></strong>`,
+    icon: "info",
+    html: `
+      <b>
+      Total = A + B
+      </b>
+      <br />
+      <br />
+      <b>
+      A: Future Value dari Aset Awal
+      </b>
+      <h2>
+      A = P (1 + <sup>r</sup>&frasl;<sub>12</sub>)<sup>12t</sup>
+      </h2>
+
+      <br />
+
+      <b>
+      B: Future Value dari Tabungan Bulanan
+      </b>
+      <h2>
+      B = PMT  * ((1 + <sup>r</sup>&frasl;<sub>12</sub>)<sup>12t</sup> - 1)/ (<sup>r</sup>&frasl;<sub>12</sub>)
+      </h2>
+      
+      `,
+    focusConfirm: false,
+    confirmButtonText: `
+       Okay
+      `,
+  });
+};
+
 function EmergencyFundPage() {
-  return <div>Emergency Fund Page</div>;
+  return (
+    <div className="sm:border-2 p-4 sm:p-8 lg:p-16 rounded-2xl mb-8">
+      <h1 className="text-2xl font-bold mb-8">
+        Hitung <span className="text-primary-blue"> Dana Darurat</span> Minimal
+        Yang Anda Butuhkan
+      </h1>
+      <form>
+        <div className="flex flex-col mb-8">
+          <label htmlFor="" className="font-bold text-xl mb-3">
+            Apa status Anda?
+          </label>
+          <div className="flex items-center">
+            <button className="bg-primary-blue px-8 py-2 text-white rounded-2xl text-lg font-semibold mr-4 ">
+              Tidak/ Belum Menikah
+            </button>
+            <button className="bg-primary-blue px-8 py-2 text-white rounded-2xl text-lg font-semibold mr-4 ">
+              Sudah Menikah
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col mb-8">
+          <label htmlFor="" className="font-bold text-xl mb-3">
+            Apakah mungkin Anda punya tanggungan lain, seperti anak, orangtua,
+            ataupun kerabat?
+          </label>
+          <div className="flex items-center">
+            <button className="bg-primary-blue px-8 py-2 text-white rounded-2xl text-lg font-semibold mr-4 ">
+              Tidak/ Belum Punya
+            </button>
+            <button className="bg-primary-blue px-8 py-2 text-white rounded-2xl text-lg font-semibold mr-4 ">
+              Punya
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col mb-8">
+          <label htmlFor="" className="font-bold text-xl mb-3">
+            Berapa Pengeluaran Bulanan Anda?
+          </label>
+          <div className="flex items-center">
+            <p className="font-bold text-xl mr-4">Rp</p>
+            <input
+              type="text"
+              placeholder="Contoh: 3.000.000"
+              className="w-3/5 px-4 py-2 border-primary-blue border-[3px] rounded-2xl"
+            />
+          </div>
+        </div>
+
+        <div className="mb-4">
+          <button className="bg-primary-blue px-8 py-2 text-white rounded-2xl text-lg font-semibold mr-4 mb-4">
+            Hitung
+          </button>
+          <button
+            onClick={descFormulaHandler}
+            className="bg-primary-blue px-8 py-2 text-white rounded-2xl text-lg font-semibold mb-4"
+          >
+            Cara Kami Menghitung?
+          </button>
+        </div>
+      </form>
+
+      <h1 className="font-bold text-2xl mb-12">
+        Dana Darurat Minimal Anda adalah Rp 18.000.000{" "}
+      </h1>
+
+      <button className="bg-primary-blue px-8 py-2 text-white rounded-2xl text-lg font-semibold mr-4 mb-8">
+        Simpan ke Tabel
+      </button>
+
+      <div className="mb-8">
+        <h1 className="font-bold text-2xl mb-4">Tabel Penyimpanan Data</h1>
+        <Table>
+          <TableCaption>Daftar tersimpan perhitungan investasi.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">No.</TableHead>
+              <TableHead>Menikah</TableHead>
+              <TableHead>Tanggungan</TableHead>
+              <TableHead>Bulanan</TableHead>
+              <TableHead className="text-right">Hasil</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>Belum Menikah</TableCell>
+              <TableCell>Tidak/ Belum Punya</TableCell>
+              <TableCell>Rp 3.000.000</TableCell>
+              <TableCell className="text-right">Rp 18.000.000</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 }
 
 export default EmergencyFundPage;
