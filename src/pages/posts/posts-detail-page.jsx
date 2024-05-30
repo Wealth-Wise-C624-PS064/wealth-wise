5dd76d5cfb4323eb2b4edf33eddc64027d38d831
Nelan <49295389+NelanJoe@users.noreply.github.com>
2024-05-30 09:26:06 +0700

    Feat comment  (#17)
    * feat: Slicing homepage, and layouting
    
    * add forum app
    
    * feat: landing-page
    
    * feat: navbar version
    
    * feat: mobile version
    
    * add page login, register
    
    * feat authentication firebase
    
    * add post firebase
    
    * feat: comment
    
    ---------
    
    Co-authored-by: Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>

--- src/pages/posts/posts-detail-page.jsx
+++ src/pages/posts/posts-detail-page.jsx
@@ -1,38 +1,58 @@
+import { useRef } from "react";
 import { useParams } from "react-router-dom";
+import { Loader2Icon } from "lucide-react";
+
+import { usePost } from "@/hooks";
 
 import BaseLayout from "@/layouts/base-layout";
-import { usePost } from "@/hooks";
 
 export default function PostsDetailPage() {
   const { postId } = useParams();
 
-  const { data: post, isSuccess, isLoading, isError, error } = usePost(postId);
-
-  let content;
-
-  if (isLoading) {
-    content = <div>Loading...</div>;
-  }
-
-  if (isSuccess) {
-    content = (
-      <article className="prose prose-a:text-blue-600 hover:prose-a:text-blue-500">
-        <h3 className="text-4xl font-semibold capitalize">{post.title}</h3>
-        <div className="prose-stone">
-          <p className="">{post.body}</p>
-        </div>
-      </article>
-    );
-  }
-
-  if (isError) {
-    content = <div>Error: {error.message}</div>;
-  }
+  const { data, isSuccess, isLoading, isError, error } = usePost(postId);
 
   return (
     <BaseLayout>
       <section className="bg-white">
-        <div className="max-w-6xl px-4 pt-6 mx-auto min-h-dvh">{content}</div>
+        <div className="max-w-6xl px-4 pt-8 pb-12 mx-auto space-y-4 min-h-dvh">
+          <div>
+            <div>
+              {isLoading && (
+                <div>
+                  <Loader2Icon className="animate-spin" />
+                </div>
+              )}
+              {isSuccess && <pre>{JSON.stringify(data, null, 2)}</pre>}
+            </div>
+          </div>
+
+          <div></div>
+
+          <div className="space-y-4">
+            <h3 className="font-medium">
+              <div className="flex flex-row gap-1">
+                Comments:
+                {isLoading && <Loader2Icon className="animate-spin" />}
+                <span>{isSuccess && data.comments.length}</span>
+              </div>
+            </h3>
+            <div>
+              {isLoading && (
+                <div>
+                  <Loader2Icon className="animate-spin" />
+                </div>
+              )}
+              {isSuccess &&
+                (data.comments.length > 0 ? (
+                  <div>{JSON.stringify(data.comments, null, 2)}</div>
+                ) : (
+                  <p>
+                    <i>Belum ada komentar</i>
+                  </p>
+                ))}
+            </div>
+          </div>
+        </div>
       </section>
     </BaseLayout>
   );
