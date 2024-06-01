46afe5f6e978e3fc2de36047ae8d57f01433c439
Nelan <49295389+NelanJoe@users.noreply.github.com>
2024-06-01 18:57:42 +0700

    Delete hooks category (#27)
    * feat: Slicing homepage, and layouting
    
    * add forum app
    
    * feat: landing-page
    
    * feat: navbar version
    
    * feat: mobile version
    
    * add page login, register
    
    * feat authentication firebase
    
    * add post firebase
    
    * feat: comment
    
    * refactor forum post and comment post
    
    * refactor posts
    
    * feat forum page
    
    * fix home page
    
    * refacor home page and forum page
    
    * remove hooks category
    
    ---------
    
    Co-authored-by: Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>

--- src/hooks/category/useCategory.js
+++
@@ -1,9 +0,0 @@
-import { getAllCategory } from "@/services/category-service";
-import { useQuery } from "@tanstack/react-query";
-
-export const useCategory = () => {
-  return useQuery({
-    queryKey: ["category"],
-    queryFn: async () => await getAllCategory(),
-  });
-};
