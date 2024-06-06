7beb0ec931cbbff43ca5207b780097c014e60f16
Vito <vitohartanto@mail.ugm.ac.id>
2024-06-06 21:54:22 +0700

    feat: add comma separator

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,7 +3,12 @@ import { useState } from "react";
 export const useInput = () => {
   const [value, setValue] = useState("");
 
-  const onChangeValueHandler = (event) => setValue(event.target.value);
+  const onChangeValueHandler = (event) => {
+    const rawValue = event.target.value.replace(/,/g, "");
+    if (!isNaN(rawValue) && /^\d*$/.test(rawValue)) {
+      setValue(rawValue.replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    }
+  };
 
   return [value, onChangeValueHandler, setValue];
 };
