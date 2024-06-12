fb28628bc315dc65b9edaa2f62c4bed1000201c7
Vito <vitohartanto@mail.ugm.ac.id>
2024-06-13 06:52:10 +0700

    feat: animations on investment page

--- src/pages/calculator/emergency-fund-page.jsx
+++ src/pages/calculator/emergency-fund-page.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useEffect } from "react";
+import { useMemo } from "react";
 
 import { useCurrentUser, useEmergency } from "@/hooks";
 
@@ -15,6 +15,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import EmergencyFundForm from "@/components/emergency-fund-form";
+import { Fade } from "react-awesome-reveal";
 
 function EmergencyFundPage() {
   const { currentUser } = useCurrentUser();
@@ -30,60 +31,62 @@ function EmergencyFundPage() {
   }, [emergencies, user]);
 
   return (
-    <div className="p-4 mb-8 sm:border-2 sm:p-8 lg:p-16 rounded-2xl">
-      <h1 className="mb-8 text-2xl font-bold">
-        Hitung <span className="text-primary-blue"> Dana Darurat</span> Minimal
-        Yang Anda Butuhkan
-      </h1>
+    <Fade delay={1e1} direction={"up"} triggerOnce={true} damping={1e-1}>
+      <div className="p-4 mb-8 sm:border-2 sm:p-8 lg:p-16 rounded-2xl">
+        <h1 className="mb-8 text-2xl font-bold">
+          Hitung <span className="text-primary-blue"> Dana Darurat</span>{" "}
+          Minimal Yang Anda Butuhkan
+        </h1>
 
-      <div>
-        <EmergencyFundForm />
-      </div>
-
-      {currentUser && (
-        <div className="mb-8">
-          <h1 className="mb-4 text-2xl font-bold">Tabel Penyimpanan Data</h1>
-          <Table>
-            <TableCaption>
-              Daftar tersimpan perhitungan dana darurat.
-            </TableCaption>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[100px]">No.</TableHead>
-                <TableHead>Menikah</TableHead>
-                <TableHead>Tanggungan</TableHead>
-                <TableHead>Bulanan</TableHead>
-                <TableHead className="text-right">Hasil</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {emergencyFilter &&
-                emergencyFilter?.map((emergency, index) => (
-                  <TableRow key={emergency.id}>
-                    <TableCell className="font-medium">{index + 1}</TableCell>
-                    <TableCell>
-                      {emergency?.menikah === "sudahMenikah"
-                        ? "Sudah Menikah"
-                        : "Tidak / Belum Menikah"}
-                    </TableCell>
-                    <TableCell>
-                      {emergency?.tanggungan === "tidakPunya"
-                        ? "Tidak / Belum Punya"
-                        : "Punya"}
-                    </TableCell>
-                    <TableCell>
-                      {emergency?.bulanan && toRupiah(emergency.bulanan)}
-                    </TableCell>
-                    <TableCell className="text-right">
-                      {toRupiah(emergency.hasil)}
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
+        <div>
+          <EmergencyFundForm />
         </div>
-      )}
-    </div>
+
+        {currentUser && (
+          <div className="mb-8">
+            <h1 className="mb-4 text-2xl font-bold">Tabel Penyimpanan Data</h1>
+            <Table>
+              <TableCaption>
+                Daftar tersimpan perhitungan dana darurat.
+              </TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">No.</TableHead>
+                  <TableHead>Menikah</TableHead>
+                  <TableHead>Tanggungan</TableHead>
+                  <TableHead>Bulanan</TableHead>
+                  <TableHead className="text-right">Hasil</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {emergencyFilter &&
+                  emergencyFilter?.map((emergency, index) => (
+                    <TableRow key={emergency.id}>
+                      <TableCell className="font-medium">{index + 1}</TableCell>
+                      <TableCell>
+                        {emergency?.menikah === "sudahMenikah"
+                          ? "Sudah Menikah"
+                          : "Tidak / Belum Menikah"}
+                      </TableCell>
+                      <TableCell>
+                        {emergency?.tanggungan === "tidakPunya"
+                          ? "Tidak / Belum Punya"
+                          : "Punya"}
+                      </TableCell>
+                      <TableCell>
+                        {emergency?.bulanan && toRupiah(emergency.bulanan)}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        {toRupiah(emergency.hasil)}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </div>
+        )}
+      </div>
+    </Fade>
   );
 }
 
