d46ea44418c78bc7f6415b59dd0141da358f15ae
vitohartanto <72955373+vitohartanto@users.noreply.github.com>
2024-06-13 07:19:17 +0700

    feat: animations on all page (#49)
    * feat: adding calculator routing
    
    * fix: missing / on path
    
    * feat: adding calculator page
    
    * feat: changing header of calculator page
    
    * feat: changing responsiveness of header calculator page
    
    * feat: creating UI for the calculator's option link
    
    * feat: slicing UI Investment Page
    
    * feat: giving responsiveness to investment page
    
    * feat: creating part of emergency-fund-page
    
    * feat: finishing emergency fund page
    
    * feat: additional changes in emergency fund page
    
    * feat: creating pension fund page
    
    * fixing UI for pension fund page
    
    * feat: create logics for investment page
    
    * giving logics to emergencyFundPage and pensionFundPage
    
    * fix: fix bug in calculating pensionFund
    
    * feat: adding profile Vito
    
    * feat: add comma separator
    
    * fix: fixing table emergency fund
    
    * fix: table and comma seperator in investment page and pension fund
    
    * feat: add icon image and react awesome reveal
    
    * feat: adding animation in hero jsx
    
    * feat: add animation in homepage
    
    * feat: animation in homepage done
    
    * feat: animation in calculator page
    
    * feat: animations on investment page
    
    * feat: animation in pension fund
    
    * feat: animations on forum page
    
    * feat: animations on article page
    
    * feat: animations on about page

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
   return (
     <section className="flex flex-col items-center justify-between max-w-6xl px-4 mx-auto my-10 md:gap-10 md:flex-row md:min-h-dvh md:my-0">
       <div className="text-center md:text-left md:w-1/2">
-        <Fade delay={1e2} direction={"left"} triggerOnce={true} damping={1e-1}>
+        <Fade delay={1e2} triggerOnce={true} damping={1e-1}>
           <h1 className="text-3xl font-bold leading-relaxed md:text-4xl md:leading-normal lg:leading-normal">
             Raih Financial Freedom
             <br />
@@ -31,7 +31,7 @@ export default function Hero() {
       </div>
 
       <div>
-        <Fade delay={1e2} direction={"right"} triggerOnce={true} damping={1e-1}>
+        <Fade delay={1e2} triggerOnce={true} damping={1e-1}>
           <img
             src="assets/banknote.svg"
             alt="banknote"
--- src/pages/forum/forum-page.jsx
+++ src/pages/forum/forum-page.jsx
@@ -14,6 +14,7 @@ import SkeletonLoaderCategories from "@/components/skeleton-loader-categories";
 
 import { Button } from "@/components/ui/button";
 import SkeletonLoaderPosts from "@/components/skeleton-loader-posts";
+import { Fade } from "react-awesome-reveal";
 
 export default function ForumPage() {
   const [searchParams] = useSearchParams();
@@ -92,65 +93,86 @@ export default function ForumPage() {
       <section className="bg-primary-blue">
         <div className="flex flex-col items-center justify-between h-full max-w-6xl px-4 py-12 mx-auto md:gap-20 md:flex-row min-h-96">
           <div className="space-y-4">
-            <h2 className="text-3xl font-semibold leading-snug text-white md:text-5xl">
-              Forum Diskusi
-            </h2>
-            <p className="text-2xl text-white">
-              Lagi pengen bahas apa hari ini?
-            </p>
+            <Fade
+              delay={1e2}
+              direction={"left"}
+              triggerOnce={true}
+              damping={1e-1}
+            >
+              <h2 className="text-3xl font-semibold leading-snug text-white md:text-5xl">
+                Forum Diskusi
+              </h2>
+              <p className="text-2xl text-white">
+                Lagi pengen bahas apa hari ini?
+              </p>
+            </Fade>
           </div>
           <div>
-            <img
-              src="/assets/discussion.svg"
-              alt="disscussion"
-              loading="lazy"
-              className="object-cover w-full md:w-[450px]"
-            />
+            <Fade
+              delay={1e2}
+              direction={"right"}
+              triggerOnce={true}
+              damping={1e-1}
+            >
+              <img
+                src="/assets/discussion.svg"
+                alt="disscussion"
+                loading="lazy"
+                className="object-cover w-full md:w-[450px]"
+              />
+            </Fade>
           </div>
         </div>
       </section>
       <section className="max-w-6xl px-4 mx-auto mt-20 mb-6 md:mb-12">
-        <div className="flex flex-row justify-between mb-2">
-          <div className="flex flex-col gap-2 mb-4">
-            <h2 className="text-xl font-semibold md:text-2xl">
-              Diskusi Aktif<span className="text-primary-blue">.</span>
-            </h2>
-            <div className="h-[2px] w-36 bg-primary-blue"></div>
-          </div>
-          <div>
-            {currentUser && (
-              <Button
-                asChild
-                className="w-full rounded-full bg-primary-blue hover:bg-primary-blue/80"
-              >
-                <Link to="/posts/create">
-                  <div className="flex flex-row items-center gap-1">
-                    <SquarePlusIcon className="w-5 h-5" />
-                    <span>Buat Diskusi Baru</span>
-                  </div>
-                </Link>
-              </Button>
-            )}
-          </div>
-        </div>
-        <div className="w-full">
-          <SearchThread />
-        </div>
-      </section>
-      <section className="max-w-6xl px-4 mx-auto mb-20">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-3 md:gap-6">
-          <div className="order-1 md:order-0 md:col-span-2">{contentPosts}</div>
-          <div className="order-0 md:order-1 md:col-span-1">
-            <div className="p-4 bg-white border rounded-md shadow-sm">
-              <div className="flex flex-col gap-2 mb-4">
-                <h2 className="text-xl font-semibold">Kategori Popular</h2>
-                <div className="bg-primary-blue w-40 h-[2px]"></div>
-              </div>
-              {contentCategories}
+        <Fade delay={1e2} triggerOnce={true} damping={1e-1}>
+          <div className="flex flex-row justify-between mb-2">
+            <div className="flex flex-col gap-2 mb-4">
+              <h2 className="text-xl font-semibold md:text-2xl">
+                Diskusi Aktif<span className="text-primary-blue">.</span>
+              </h2>
+              <div className="h-[2px] w-36 bg-primary-blue"></div>
+            </div>
+            <div>
+              {currentUser && (
+                <Button
+                  asChild
+                  className="w-full rounded-full bg-primary-blue hover:bg-primary-blue/80"
+                >
+                  <Link to="/posts/create">
+                    <div className="flex flex-row items-center gap-1">
+                      <SquarePlusIcon className="w-5 h-5" />
+                      <span>Buat Diskusi Baru</span>
+                    </div>
+                  </Link>
+                </Button>
+              )}
             </div>
           </div>
-        </div>
+          <div className="w-full">
+            <SearchThread />
+          </div>
+        </Fade>
+      </section>
+      <section className="max-w-6xl px-4 mx-auto mb-20">
+        <Fade delay={1e2} triggerOnce={true} damping={1e-1}>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-3 md:gap-6">
+            <div className="order-1 md:order-0 md:col-span-2">
+              {contentPosts}
+            </div>
+            <div className="order-0 md:order-1 md:col-span-1">
+              <div className="p-4 bg-white border rounded-md shadow-sm">
+                <div className="flex flex-col gap-2 mb-4">
+                  <h2 className="text-xl font-semibold">Kategori Popular</h2>
+                  <div className="bg-primary-blue w-40 h-[2px]"></div>
+                </div>
+                {contentCategories}
+              </div>
+            </div>
+          </div>
+        </Fade>
       </section>
     </BaseLayout>
   );
 }
+
